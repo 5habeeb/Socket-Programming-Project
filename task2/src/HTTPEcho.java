3099def160ca593a6b9a68649791eb787af52159
5habeeb <ahmadshabeeb93@gmail.com>
2018-02-21 13:13:24 +0100

    task2 modified to match the requirements of the testing software

--- task2/src/HTTPEcho.java
+++ task2/src/HTTPEcho.java
@@ -4,76 +4,53 @@ import java.io.*;
 public class HTTPEcho {
     private static ServerSocket server;
 
-    public static void main( String[] args) {
+    public static void main(String[] args) {
         int port = Integer.parseInt(args[0]);
 
         try {
             server = new ServerSocket(port);
             System.out.println("server is running \t waiting for connection...");
         } catch (IOException e) {
+            System.out.println("Cannot listen to port " + port);
             e.printStackTrace();
         }
 
         while (true) {
             try {
-                handleClient();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+                // Accept and establish a connection with a client
+                Socket connectionSocket;
+                connectionSocket = server.accept();
+                System.out.println("Got a connection");
 
-    private static void handleClient() throws IOException {
-        Socket connectionSocket;
-        String HTTPRequest = null;
+                // Get the data from the client
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 
-        // accept and establish a connection with a client
-        connectionSocket = server.accept();
-        System.out.println("Got a connection");
-        connectionSocket.setSoTimeout(2000);
-
-        // Get the data from the client
-        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-
-        // read line by line from the BufferReader
-        try{
-            long timeToStop = System.currentTimeMillis()+ 2000;
-            String line;
-            StringBuilder resultBuilder = new StringBuilder("");
-            while((line = inFromClient.readLine()) != null ){
-                if(line.isEmpty())
-                    resultBuilder.append("\n");
-                else
-                    resultBuilder.append(line).append("\n");
-
-                // update the result
-                HTTPRequest = resultBuilder.toString();
-
-                // check the timer
-                if(System.currentTimeMillis() > timeToStop){
-                    throw new SocketTimeoutException();
+                String line = inFromClient.readLine();
+                StringBuilder resultBuilder = new StringBuilder("");
+                while (!line.isEmpty()) {
+                    resultBuilder.append(line).append("\r\n");
+                    line = inFromClient.readLine();
                 }
+
+                System.out.println("Client says: " + resultBuilder.toString());
+
+                // generate the HTTP response
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + resultBuilder.toString();
+
+                PrintWriter outToClient = new PrintWriter(connectionSocket.getOutputStream(), true);
+                outToClient.println(httpResponse);
+
+                System.out.println("Sent to client: " + httpResponse);
+
+                //close connection
+                connectionSocket.close();
+                System.out.println("connection closed");
+
+            } catch (IOException e) {
+                System.err.println("Error.");
+                System.exit(1);
             }
-        }catch (SocketTimeoutException e){
-            // show the HTTP request sent by the client
-            System.out.println( "client says: \n" + HTTPRequest);
 
-            // generate a HTTP response
-            PrintWriter outToClient = new PrintWriter(connectionSocket.getOutputStream(), true);
-            outToClient.println("HTTP/1.1 200 ok");
-            outToClient.println("Content-type: text/plain");
-            outToClient.println("\r\n");
-            outToClient.println(HTTPRequest);
-
-            //close connection
-            connectionSocket.close();
-            System.out.println("connection closed");
         }
-
     }
-
-
-
-
 }
-
